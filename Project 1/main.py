6ddb69c54eb8da1f9089d406d97708c9d828c0f8
Yash Mishra <myash0370@gmail.com>
2026-03-18 20:03:41 +0530

    mini project

---
+++ Project 1/main.py
@@ -0,0 +1,35 @@
+# PROJECT 1: SNAKE, WATER, GUN GAME:
+import random 
+computer = random.choice([-1 , 1 , 0])
+user_input = input("Enter your choice: ")
+user_dict = {"s": 1, "w":-1, "g":0}
+reverse_dict = {1:"Snake", -1:"Water", 0:"Gun"}
+
+user = user_dict[user_input]
+
+print(f"You chose {reverse_dict[user]}\nComputer chose {reverse_dict[computer]}")
+
+if(computer == user):
+    print("Its a Draw")
+
+else:
+    if(computer == -1 or user == 1):
+        print("Congratulations\nYou win!")
+    
+    elif(computer == -1 or user == 0):
+        print("You Lose!")
+
+    elif(computer == 1 or user == -1):
+        print("You Lose!")
+
+    elif(computer == 1 or user == 0):
+        print("Congratulations\nYou win!")
+
+    elif(computer == 0 or user ==-1):
+        print("Congratulations\nYou win!")
+
+    elif(computer == 0 or user == 1):
+        print("You Lose!")
+    
+    else:
+        print("Something went wrong")
