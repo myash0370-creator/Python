373e27c57f95ca0dfbd60784aea9096336215d07
Yash Mishra <myash0370@gmail.com>
2026-03-28 12:22:08 +0530

    practice

---
+++ Advance Python 1 practise set/practice4.py
@@ -0,0 +1,7 @@
+# Write a program to display a/b where a and b are integers. If b=0, display infinite by handling the ‘ZeroDivisionError’.
+try:
+    a = int(input("Enter the number a: "))
+    b = int(input("Enter the number b:"))
+    print(a/b)
+except ZeroDivisionError as v:
+    print("Infinite")
